e0f24302317d3cbb6c76a69451f0df3d1ab80b7b
Julia <g.julia82@gmail.com>
2019-11-05 10:02:17 +0200

    Separation of a project on the classes: TestBase and DraftTest

---
+++ Project1/test/DraftTest.cs
@@ -0,0 +1,27 @@
+﻿using DotLiquid.Tags;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    
+    class DraftTest: TestBase
+    {      
+        [Test]
+        public void DraftVerification()
+    {
+        SignInAccount();
+        GoToGmail();
+        GoToDraftFolder();
+        CreateDraft();
+        UpdateDraft();
+        SingOut();
+    }
+    
+    }
+}
+
+
